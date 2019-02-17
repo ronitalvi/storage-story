fab4507b7634c64193402ad26a12dc62355819f7
roni talvi <vtalvi@gmail.com>
2019-02-17 17:10:36 +0200

    validations vol2

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,6 @@ class Booking < ApplicationRecord
   has_many :reviews
   has_many :mesages
 
-  validates :star_date, presence: true
+  validates :start_date, presence: true
   validates :end_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ApplicationRecord
   has_many :reviews, through: :bookings ### ?
 
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+
 end
