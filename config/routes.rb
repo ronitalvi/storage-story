0664d971a497997a388d08c5747fce465d06f910
roni talvi <vtalvi@gmail.com>
2019-02-17 18:25:49 +0200

    last push before generate divice view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'pages#home'
   resources :users do
   end
 
