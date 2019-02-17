0c00c3e6d4ce3db16d81759714754441306aefe1
roni talvi <vtalvi@gmail.com>
2019-02-17 16:16:02 +0200

    Validations - done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :stars, inclusion: { in: %w[0 1 2 3 4 5] }
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  has_many :mesages, through: :bookings ### whith through or not
+  has_many :reviews, through: :bookings ### ?
+
+
+  validates :name, presence: true, uniqueness: true
 end
