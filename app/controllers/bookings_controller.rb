a42ef643e98ee583672c9f8f91c00bc9d0d5c8ea
d-mv <d.dash.mv@gmail.com>
2019-02-18 14:54:55 +0200

    Added new Booking feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-    before_action :index
+
   def index
     @bookings = Booking.all
     @my_storage_bookings = []
@@ -15,8 +15,23 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
+    @storage = Storage.find(params['storage_id'])
   end
 
   def create
+    booking = Booking.new(
+      storage_id: params[:storage_id],
+      start_date: params[:booking][:start_date],
+      end_date: params[:booking][:end_date],
+      approved: false,
+      user_id: current_user.id
+    )
+    booking.save!
+    if booking.save!
+      redirect_to Storage.find(params[:storage_id]), notice: 'storage was successfully booked.'
+    else
+      render :new
+    end
   end
 end
