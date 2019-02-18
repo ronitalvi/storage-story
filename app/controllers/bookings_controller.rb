84a2bdaf01f2acd1339f1e48f1180c01c0be34f9
d-mv <d.dash.mv@gmail.com>
2019-02-18 12:16:11 +0200

    Bookings showing only my rents and my storage bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,22 @@
 class BookingsController < ApplicationController
-    before_action :show
+    before_action :index
   def index
+    @bookings = Booking.all
+    @my_storage_bookings = []
+    @bookings.each do |booking|
+      if booking.storage.user_id == current_user.id
+        @my_storage_bookings << booking
+      end
+    end
+    @my_rents = Booking.where(user: current_user)
   end
 
   def show
-    @bookings = Booking.all
   end
 
   def new
-
   end
-  
-  def create
 
+  def create
   end
 end
