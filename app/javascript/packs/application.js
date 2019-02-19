f278245d1acffdc40e5dff4aaea678f8b1e9fbf1
d-mv <d.dash.mv@gmail.com>
2019-02-19 16:18:28 +0200

    Corrections to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
-import "bootstrap";
-
+// import "bootstrap";
 import './storage/index';
 import './message/index';
 import './message/show';
@@ -9,14 +8,6 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import {initAutocomplete} from '../plugins/init_autocomplete'
 import { initMapbox } from '../plugins/init_mapbox';
-
-
-import './storage/index';
-
-import './messages/index';
-
-
-
 
 initMapbox();
 initAutocomplete()
