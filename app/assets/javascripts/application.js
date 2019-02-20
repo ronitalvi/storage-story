3870cce062cf094c415f8a3be7051530e45d332f
mathias knudsen <mathias.knudsen31@gmail.com>
2019-02-20 18:49:01 +0200

    hey guys

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require rails-ujs
 //= require_tree .
-<<<<<<< HEAD
 //= require jquery
 //= require bootstrap-sprockets
-=======
 //= require hide
->>>>>>> 10e1ca1f0fe055a76158ab653af8b2670f233ffc
--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -4,7 +4,6 @@ class StoragesController < ApplicationController
 
   def index
     @storages = Storage.all
-
     @markers = @storages.map do |storage|
       {
         lng: storage.longitude,
